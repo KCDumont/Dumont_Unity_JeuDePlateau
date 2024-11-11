dd4c3bf13fa5dbb5d0ee3d18c980599f9740c817
KCDumont <kaili.dumont@gmail.com>
2024-11-11 19:13:33 +0100

    Anim Saut Essai
    11/11  -  19:12
    Tentative de faire fonctionner l'animation saut. Avant changement, l'anim se lance mais ne s'arrête pas.

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -8,17 +8,37 @@ public class NPCController : MonoBehaviour
     [SerializeField] private GameObject _target;
     private NavMeshAgent _agent;
     private Animator _animator;
+    //private bool IsJumping = false;
+    private bool _jumping = false;
 
     private void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
         _animator = GetComponent<Animator>();
         _agent.SetDestination(_target.transform.position);
+
+        //if (!IsJumping)
+        if (_jumping)
+        {
+            _jumping = false;
+            _animator.SetBool("IsJumping", _jumping);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _jumping = true;
+            _animator.SetBool("IsJumping", _jumping);
+        }
     }
 
     private void Update()
     {
         float speed = _agent.velocity.magnitude;
         _animator.SetFloat("Speed", speed);
+        _jumping=true;
+        //Find a way to set the magnitude up so it jumps
+       // _animator.SetBool("IsJumping", isJumping);
+        //if (_jumping = true) ;
+
+      
     }
 }
